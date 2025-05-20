781733dfe414b3fbb1339e0dd548a1e9c30bf188
Rhana <159554135+RHAN4@users.noreply.github.com>
2025-05-20 11:27:55 -0300

    Add files via upload

---
+++ api/src/main/java/com/example/aula/exception/NomeJaCadastradoException.java
@@ -0,0 +1,7 @@
+package com.example.aula.exception;
+
+public class NomeJaCadastradoException extends RuntimeException {
+    public NomeJaCadastradoException(String message) {
+        super(message);
+    }
+}
---
+++ bancoDeDados.sql
@@ -0,0 +1,9 @@
+create database if not exists db_projeto;
+
+use db_projeto;
+
+show tables;
+
+alter table usuario drop column email;
+
+alter table usuario drop column senha;
